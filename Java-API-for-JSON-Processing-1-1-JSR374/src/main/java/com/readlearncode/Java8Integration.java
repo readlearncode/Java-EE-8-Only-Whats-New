2bedc6b25550016d841d8a07db6f19056484fa18
Alex Theedom <alex.theedom@gmail.com>
2018-02-06 19:52:22 +0000

    Add JSON P examples

---
+++ Java-API-for-JSON-Processing-1-1-JSR374/src/main/java/com/readlearncode/Java8Integration.java
@@ -0,0 +1,31 @@
+package com.readlearncode;
+
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonString;
+import javax.json.stream.JsonCollectors;
+import java.io.StringReader;
+
+/**
+ * Source code github.com/readlearncode
+ *
+ * @author Alex Theedom www.readlearncode.com
+ * @version 1.0
+ */
+public class Java8Integration {
+
+    public static void main(String... args) {
+
+        String jsonData = "{\"tags\":[\"JAX-RS\",\"JSF\",\"Security\",\"JSP\",\"EJB\",\"JMS\",\"Bean Validation\",\"Websocket\",\"JPA\"]}";
+        JsonObject jsonObject = Json.createReader(new StringReader(jsonData)).readObject();
+
+        JsonArray tags = jsonObject.getJsonArray("tags").stream()
+                .filter(jsonValue -> ((JsonString) jsonValue).getString().startsWith("J"))
+                .collect(JsonCollectors.toJsonArray());
+
+        System.out.println(tags);
+
+    }
+
+}
--- Java-API-for-JSON-Processing-1-1-JSR374/src/main/java/com/readlearncode/streaming/JsonParserInterface.java
+++ Java-API-for-JSON-Processing-1-1-JSR374/src/main/java/com/readlearncode/streaming/JsonParserInterface.java
@@ -40,5 +40,6 @@ public class JsonParserInterface {
             }
         }
 
+
     }
 }
