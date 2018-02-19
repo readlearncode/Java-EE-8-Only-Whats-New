d49d27af02b8c0790e24f996f6ebbb12c355aa07
Alex Theedom <alex.theedom@gmail.com>
2018-02-19 12:43:58 +0000

    Add reactive JAX-RS examples

---
+++ Java-API-for-RESTful-WebServices-2-1-JSR370/src/main/java/com/readlearncode/reactive/BookResource.java
@@ -0,0 +1,33 @@
+package com.readlearncode.reactive;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+/**
+ * Source code github.com/readlearncode
+ *
+ * @author Alex Theedom www.readlearncode.com
+ * @version 1.0
+ */
+@Path("/books")
+public class BookResource {
+
+    @Inject
+    private BookRepository bookRepository;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllBooks() {
+        List<Book> books = bookRepository.getAllBooks();
+        GenericEntity<List<Book>> list = new GenericEntity<List<Book>>(books) {
+        };
+        return Response.ok(list).build();
+    }
+
+}
